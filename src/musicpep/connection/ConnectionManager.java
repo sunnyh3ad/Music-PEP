33ef0da6027c96a636762ce7c1380c2aaf3f6936
Nina Vdovicenco <nvdovic1@umbc.edu>
2023-11-01 10:26:31 -0400

    added ConnectionManager

---
+++ src/musicpep/connection/ConnectionManager.java
@@ -0,0 +1,60 @@
+package musicpep.connection;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class ConnectionManager {
+	
+	// ?serverTimezone=EST5EDT <-- add to end of URL if trouble connecting and on Mac/Linux
+	private static final String URL = "jdbc:mysql://localhost:3306/musictracker";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
+	private static Connection connection = null;
+	
+
+	private static void makeConnection() throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+	}
+
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+
+		if (connection == null) {
+			makeConnection();
+		}
+
+		return connection;
+	}
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		
+		try {
+			conn = ConnectionManager.getConnection();
+			System.out.println("Connected");
+			
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			conn.close();
+			System.out.println("Closed connection");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
